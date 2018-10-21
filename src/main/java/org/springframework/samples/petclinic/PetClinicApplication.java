aa2b6ed86f513cbfe01391811735374ae5191440
wonwoo <aoruqjfu@gmail.com>
2018-10-21 15:39:52 +0900

    rename class

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PetclinicApplication {
+public class PetClinicApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PetclinicApplication.class, args);
+        SpringApplication.run(PetClinicApplication.class, args);
     }
 }
